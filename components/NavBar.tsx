fd5c0e9c0fba18cb748d212c6f3e45d1b1b0866f
Hitigome <kawibrian1@gmail.com>
2024-05-14 15:49:06 -0700

    Revert signup changes

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,10 @@
 import AuthButton from "../components/AuthButton";
 import DeployButton from "../components/DeployButton";
-import { createClient } from "@/utils/supabase/server";
 
 export default function NavBar() {
     return (
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-            <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+            <div className="w-full flex justify-between items-center p-3 text-sm">
                 <DeployButton />
                 <AuthButton />
             </div>
