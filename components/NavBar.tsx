9765d4a0c78485ace0f1bff3068625394affe336
Hitigome <kawibrian1@gmail.com>
2024-05-15 03:52:08 -0700

    Change deploy button on navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,7 +5,7 @@ export default function NavBar() {
     return (
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
             <div className="w-full flex justify-between items-center p-3 text-sm">
-                <DeployButton />
+                <a href='/'>Tuned Nation</a>
                 <AuthButton />
             </div>
         </nav>
